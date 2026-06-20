f6b3548f2efbde4cb471f933728f75b1654467b7
Anu <anumostech@gmail.com>
2026-06-20 11:27:46 +0530

    conflicts solved

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-use Tymon\JWTAuth\Contracts\JWTSubject;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
 use Illuminate\Database\Eloquent\SoftDeletes;
 
